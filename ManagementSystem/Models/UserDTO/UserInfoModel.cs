57689d3e74cfded303d6f41c0b563bdbcbbca536
Asya0Anastasiya <nastia.rynkevich@gmail.com>
2024-02-08 14:14:34 +0300

    role was added to userInfoModel

--- ManagementSystem/Models/UserDTO/UserInfoModel.cs
+++ ManagementSystem/Models/UserDTO/UserInfoModel.cs
@@ -4,30 +4,22 @@ namespace UserService.Models.UserDto
 {
     public class UserInfoModel
     {
-        [Required]
         public Guid Id { get; set; }
 
-        [Required]
         public string FirstName { get; set; }
 
-        [Required]
         public string LastName { get; set; }
 
-        [Required]
         public string Email { get; set; }
+
+        public string Role { get; set; }
 
         public string PhoneNumber { get; set; }
 
-        [Required]
-        [MaxLength(30)]
         public string Department { get; set; }
 
-        [Required]
-        [MaxLength(30)]
         public string Position { get; set; }
 
-        [Required]
-        [MaxLength(30)]
         public string BranchOffice { get; set; }
     }
 }
